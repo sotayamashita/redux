2ddaba017ce69e1fcb3f229574f076d5bee5241a
Sota Yamashita <sota.yamashita@gmail.com>
2016-04-05 21:34:27 +0900

    setup

--- examples/todos/components/App.js
+++ examples/todos/components/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Footer from './Footer'
 import AddTodo from '../containers/AddTodo'
 import VisibleTodoList from '../containers/VisibleTodoList'
+import DevTools from '../containers/DevTools';
 
 const App = () => (
   <div>
     <AddTodo />
     <VisibleTodoList />
     <Footer />
+    <DevTools/> 
   </div>
 )
 
--- examples/todos/index.js
+++ examples/todos/index.js
@@ -2,11 +2,17 @@ import 'babel-polyfill'
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, compose } from 'redux'
 import todoApp from './reducers'
 import App from './components/App'
 
-let store = createStore(todoApp)
+import DevTools from './containers/DevTools';
+
+const enhancer = compose(
+  DevTools.instrument()
+);
+
+let store = createStore(todoApp, enhancer)
 
 render(
   <Provider store={store}>
